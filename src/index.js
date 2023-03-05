1d4f01755977c4d7443a6f485ec1b9ac78baac20
Dhiraj sawariyastore84@gmail.com <sawariyastore84@gmail.com>
2023-03-05 19:31:29 +0530

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Game from "./component/Game";
-import "./css/index.css";
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);  
