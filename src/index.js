a358f5baeccb880eae01174a5465f01da61059d9
Dhiraj sawariyastore84@gmail.com <sawariyastore84@gmail.com>
2023-03-05 23:04:16 +0530

    add route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Game from './component/Game';
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./css/index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router>
-    <Game />
+        <Route path='/board-game-app' exact component={Game}/>
   </Router>);
