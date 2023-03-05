e9a5a27575af0177571801ffa484b141673ec467
Dhiraj sawariyastore84@gmail.com <sawariyastore84@gmail.com>
2023-03-05 22:31:53 +0530

    add router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Game from "./component/Game";
-import "./css/index.css";
-  
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Game from './component/Game';
+import { HashRouter as Router } from "react-router-dom";
+import "./css/index.css"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Router>
+    <Game />
+  </Router>);
